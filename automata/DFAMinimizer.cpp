51be0963ae14112ed9884426f3463b2dc47f696a
Mohamed El Amine Kherroubi <elaminekherroubi@gmail.com>
2025-10-10 16:25:53 +0200

    Fix minimizer

--- automata/DFAMinimizer.cpp
+++ automata/DFAMinimizer.cpp
@@ -3,64 +3,119 @@
 #include "headers/State.h"
 #include <map>
 #include <queue>
+#include <set>
 
 DFA DFAMinimizer::minimize(DFA &dfa) {
   std::map<Superstate, StateID> superstate_to_state_id_map;
   std::queue<Superstate> superstates_to_process;
-
-  States minimized_dfa_states;
-  StateIDs minimized_dfa_accepting_state_ids;
+  std::set<Superstate> processed_superstates;
+  Superstate nonaccepting_superstate, accepting_superstate;
   int next_state_id = 0;
 
-  Superstate nonaccepting_superstate, accepting_superstate;
-
+  // Initial partition: accepting vs non-accepting states
   for (State state : dfa.getStates()) {
     if (dfa.isAccepting(state.getID())) {
       accepting_superstate.insert(state.getID());
-
     } else {
       nonaccepting_superstate.insert(state.getID());
     }
   }
 
+  // Add non-accepting partition
   if (!nonaccepting_superstate.empty()) {
-    superstate_to_state_id_map[nonaccepting_superstate] = next_state_id;
-    minimized_dfa_states.push_back(State{next_state_id++});
+    superstate_to_state_id_map[nonaccepting_superstate] = next_state_id++;
     superstates_to_process.push(nonaccepting_superstate);
   }
 
+  // Add accepting partition
   if (!accepting_superstate.empty()) {
-    superstate_to_state_id_map[accepting_superstate] = next_state_id;
-    minimized_dfa_states.push_back(State{next_state_id});
-    minimized_dfa_accepting_state_ids.push_back(next_state_id++);
+    superstate_to_state_id_map[accepting_superstate] = next_state_id++;
     superstates_to_process.push(accepting_superstate);
   }
 
   Alphabet alphabet = dfa.getAlphabet();
 
-  DFA minimized_dfa{alphabet, minimized_dfa_states,
-                    minimized_dfa_accepting_state_ids, 0};
-
+  // Refine partitions
   while (!superstates_to_process.empty()) {
     Superstate current_superstate = superstates_to_process.front();
     superstates_to_process.pop();
 
-    for (Symbol symbol : alphabet) {
-      Superstate inside_superstate;
-      Superstate outside_superstate;
+    if (processed_superstates.find(current_superstate) !=
+        processed_superstates.end()) {
+      continue;
+    }
+    processed_superstates.insert(current_superstate);
 
-      for (State state : dfa.getStates()) {
-        StateID next_state_id = dfa.getNextState(state.getID(), symbol);
-        if (current_superstate.find(next_state_id) ==
-            current_superstate.end()) {
-          inside_superstate.insert(state.getID());
-        } else {
-          outside_superstate.insert(state.getID());
+    for (Symbol symbol : alphabet) {
+      std::map<StateID, Superstate> transition_map;
+
+      // Group states by their transition target partition
+      for (StateID state_id : current_superstate) {
+        StateID next_state = dfa.getNextState(state_id, symbol);
+
+        // Find which superstate contains next_state
+        for (const auto &[target_superstate, target_id] :
+             superstate_to_state_id_map) {
+          if (target_superstate.find(next_state) != target_superstate.end()) {
+            transition_map[target_id].insert(state_id);
+            break;
+          }
         }
       }
-      if (superstate_to_state_id_map.find())
-        superstates_to_process.push(inside_superstate);
-      superstates_to_process.push(outside_superstate);
+
+      // Split partition if states transition to different targets
+      if (transition_map.size() > 1) {
+        superstate_to_state_id_map.erase(current_superstate);
+
+        for (const auto &[target_partition_id, states_in_new_partition] :
+             transition_map) {
+          if (superstate_to_state_id_map.find(states_in_new_partition) ==
+              superstate_to_state_id_map.end()) {
+            superstate_to_state_id_map[states_in_new_partition] =
+                next_state_id++;
+            superstates_to_process.push(states_in_new_partition);
+          }
+        }
+      }
     }
   }
+
+  // Build minimized DFA
+  States minimized_states;
+  StateIDs minimized_accepting_ids;
+
+  for (const auto &[superstate, new_state_id] : superstate_to_state_id_map) {
+    minimized_states.push_back(State{new_state_id});
+
+    // Check if this partition contains an accepting state
+    for (StateID old_id : superstate) {
+      if (dfa.isAccepting(old_id)) {
+        minimized_accepting_ids.push_back(new_state_id);
+        break;
+      }
+    }
+  }
+
+  DFA minimized_dfa{alphabet, minimized_states, minimized_accepting_ids, 0};
+  minimized_dfa.resizeTransitions(superstate_to_state_id_map.size());
+
+  // Build transitions
+  for (const auto &[superstate, from_state_id] : superstate_to_state_id_map) {
+    StateID representative = *superstate.begin();
+
+    for (Symbol symbol : alphabet) {
+      StateID old_target = dfa.getNextState(representative, symbol);
+
+      // Find target partition
+      for (const auto &[target_superstate, target_state_id] :
+           superstate_to_state_id_map) {
+        if (target_superstate.find(old_target) != target_superstate.end()) {
+          minimized_dfa.addTransition(from_state_id, symbol, target_state_id);
+          break;
+        }
+      }
+    }
+  }
+
+  return minimized_dfa;
 }
